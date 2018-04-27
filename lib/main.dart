2aaf72d518eb1a8c7e34b353176da5190e88bdc4
Marcin Szalek <marcin.szalek1@gmail.com>
2018-04-27 16:16:02 +0200

    Added fab animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
@@ -179,18 +181,18 @@ class _MyHomePageState extends State<MyHomePage>
     );
   }
 
+//  _onFabTap() {
+//    scrollController.jumpTo(0.0);
+//    shouldOnlyCompletedBeShownNotifier.value =
+//        !shouldOnlyCompletedBeShownNotifier.value;
+//  }
+
   Widget _buildFab() {
     return new Positioned(
-        top: imageHeight - 40.0,
-        right: 20.0,
-        child: new FloatingActionButton(
-            onPressed: () {
-              scrollController.jumpTo(0.0);
-              shouldOnlyCompletedBeShownNotifier.value =
-                  !shouldOnlyCompletedBeShownNotifier.value;
-            },
-            backgroundColor: Colors.pink,
-            child: new Icon(Icons.filter_list)));
+      top: imageHeight - 100.0,
+      right: -40.0,
+      child: new FilterFab(filterValueNotifier: shouldOnlyCompletedBeShownNotifier),
+    );
   }
 
   Widget _buildTimeline() {
@@ -243,6 +245,152 @@ class _MyHomePageState extends State<MyHomePage>
   }
 }
 
+class FilterFab extends StatefulWidget {
+  final ValueNotifier<bool> filterValueNotifier;
+
+  const FilterFab({Key key, this.filterValueNotifier}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return new FilterFabState();
+  }
+}
+
+class FilterFabState extends State<FilterFab>
+    with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+  Animation<double> animation;
+  Animation<Color> colorAnimation;
+
+  final double expandedSize = 180.0;
+  final double hiddenSize = 20.0;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = new AnimationController(
+        vsync: this, duration: Duration(milliseconds: 200));
+    animation = new CurvedAnimation(
+        parent: animationController, curve: Curves.easeInOut);
+    colorAnimation = new ColorTween(begin: Colors.pink, end: Colors.pink[800])
+        .animate(animationController);
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new SizedBox(
+      width: expandedSize,
+      height: expandedSize,
+      child: new AnimatedBuilder(
+          animation: animation,
+          builder: (context, child) {
+            return new Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                _buildExpandedBackground(),
+                buildOption(Icons.check_circle, 0.0),
+                buildOption(Icons.flash_on, -math.pi / 3),
+                buildOption(Icons.access_time, -2 * math.pi / 3),
+                buildOption(Icons.error_outline, math.pi),
+                _buildFabCore(),
+              ],
+            );
+          }),
+    );
+  }
+
+  open() {
+    if (animationController.isDismissed) {
+      animationController.forward();
+    }
+  }
+
+  close() {
+    if (animationController.isCompleted) {
+      animationController.reverse();
+    }
+  }
+
+  _onFabTap() {
+    if (animation.isDismissed) {
+      open();
+    } else {
+      close();
+    }
+  }
+
+  _onItemTap() {
+    widget.filterValueNotifier.value = !widget.filterValueNotifier.value;
+    close();
+  }
+
+  Widget _buildExpandedBackground() {
+    return new Container(
+      height: hiddenSize + (expandedSize - hiddenSize) * animation.value,
+      width: hiddenSize + (expandedSize - hiddenSize) * animation.value,
+      decoration: new BoxDecoration(shape: BoxShape.circle, color: Colors.pink),
+    );
+  }
+
+  Widget _buildFabCore() {
+    double defaultIconSize = 26.0;
+    double scaleFactor = 2 * (animationController.value - 0.5).abs();
+    return new FloatingActionButton(
+      onPressed: _onFabTap,
+      backgroundColor: colorAnimation.value,
+      child: new Center(
+        child: new Transform(
+          alignment: Alignment.center,
+          transform: new Matrix4.identity()..scale(1.0, scaleFactor),
+          child: new Icon(
+            animationController.value > 0.5 ? Icons.close : Icons.filter_list,
+            color: Colors.white,
+            size: defaultIconSize,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildOption(IconData icon, double angle) {
+    double iconSize = 0.0;
+    if (animation.value > 0.5) {
+      iconSize = 26 * animation.value;
+    }
+    double defaultPadding = 16.0;
+    double padding = defaultPadding +
+        (expandedSize - defaultPadding) * (1 - animation.value);
+    return new Transform.rotate(
+      angle: angle,
+      child: new Align(
+        alignment: Alignment.topCenter,
+        child: new Padding(
+          padding: new EdgeInsets.only(top: padding),
+          child: new IconButton(
+            onPressed: _onItemTap,
+            icon: new Transform.rotate(
+              angle: -angle,
+              child: new Icon(
+                icon,
+                color: Colors.white,
+              ),
+            ),
+            iconSize: iconSize,
+            alignment: Alignment.topCenter,
+            padding: new EdgeInsets.all(0.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class TaskRow extends StatefulWidget {
   final double speedFactor;
   final ValueNotifier<bool> showCompletedNotifier;
@@ -272,7 +420,7 @@ class TaskRowState extends State<TaskRow> with TickerProviderStateMixin {
         value: 1.0,
         vsync: this,
         duration: new Duration(
-            milliseconds: (50 * (1 + 2 * widget.speedFactor)).toInt()));
+            milliseconds: (200 * (1 + 2 * widget.speedFactor)).toInt()));
     animation = new CurvedAnimation(
         parent: animationController, curve: Curves.easeInOut);
     onFilterChanged = () {
