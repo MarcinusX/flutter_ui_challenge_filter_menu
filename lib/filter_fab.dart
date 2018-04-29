f3898c76f03a1b4330871f1af6f2e73baf3b03dc
Marcin Szalek <marcin.szalek1@gmail.com>
2018-04-29 12:40:30 +0200

    Fixed list bug and refactored

---
+++ lib/filter_fab.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+
+import 'dart:math' as math;
+
+class FilterFab extends StatefulWidget {
+  final ValueNotifier<bool> filterValueNotifier;
+
+  const FilterFab({Key key, this.filterValueNotifier}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return new FilterFabState();
+  }
+}
+
+class FilterFabState extends State<FilterFab>
+    with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+  Animation<double> animation;
+  Animation<Color> colorAnimation;
+
+  final double expandedSize = 180.0;
+  final double hiddenSize = 20.0;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = new AnimationController(
+        vsync: this, duration: Duration(milliseconds: 200));
+    animation = new CurvedAnimation(
+        parent: animationController, curve: Curves.easeInOut);
+    colorAnimation = new ColorTween(begin: Colors.pink, end: Colors.pink[800])
+        .animate(animationController);
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new SizedBox(
+      width: expandedSize,
+      height: expandedSize,
+      child: new AnimatedBuilder(
+          animation: animation,
+          builder: (context, child) {
+            return new Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                _buildExpandedBackground(),
+                buildOption(Icons.check_circle, 0.0),
+                buildOption(Icons.flash_on, -math.pi / 3),
+                buildOption(Icons.access_time, -2 * math.pi / 3),
+                buildOption(Icons.error_outline, math.pi),
+                _buildFabCore(),
+              ],
+            );
+          }),
+    );
+  }
+
+  open() {
+    if (animationController.isDismissed) {
+      animationController.forward();
+    }
+  }
+
+  close() {
+    if (animationController.isCompleted) {
+      animationController.reverse();
+    }
+  }
+
+  _onFabTap() {
+    if (animation.isDismissed) {
+      open();
+    } else {
+      close();
+    }
+  }
+
+  _onItemTap() {
+    widget.filterValueNotifier.value = !widget.filterValueNotifier.value;
+    close();
+  }
+
+  Widget _buildExpandedBackground() {
+    return new Container(
+      height: hiddenSize + (expandedSize - hiddenSize) * animation.value,
+      width: hiddenSize + (expandedSize - hiddenSize) * animation.value,
+      decoration: new BoxDecoration(shape: BoxShape.circle, color: Colors.pink),
+    );
+  }
+
+  Widget _buildFabCore() {
+    double defaultIconSize = 26.0;
+    double scaleFactor = 2 * (animationController.value - 0.5).abs();
+    return new FloatingActionButton(
+      onPressed: _onFabTap,
+      backgroundColor: colorAnimation.value,
+      child: new Center(
+        child: new Transform(
+          alignment: Alignment.center,
+          transform: new Matrix4.identity()..scale(1.0, scaleFactor),
+          child: new Icon(
+            animationController.value > 0.5 ? Icons.close : Icons.filter_list,
+            color: Colors.white,
+            size: defaultIconSize,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildOption(IconData icon, double angle) {
+    double iconSize = 0.0;
+    if (animation.value > 0.5) {
+      iconSize = 26 * animation.value;
+    }
+    double defaultPadding = 16.0;
+    double padding = defaultPadding +
+        (expandedSize - defaultPadding) * (1 - animation.value);
+    return new Transform.rotate(
+      angle: angle,
+      child: new Align(
+        alignment: Alignment.topCenter,
+        child: new Padding(
+          padding: new EdgeInsets.only(top: padding),
+          child: new IconButton(
+            onPressed: _onItemTap,
+            icon: new Transform.rotate(
+              angle: -angle,
+              child: new Icon(
+                icon,
+                color: Colors.white,
+              ),
+            ),
+            iconSize: iconSize,
+            alignment: Alignment.topCenter,
+            padding: new EdgeInsets.all(0.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'dart:math' as math;
-
+import 'package:filter_menu/filter_fab.dart';
+import 'package:filter_menu/task.dart';
+import 'package:filter_menu/task_row.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
@@ -55,13 +56,12 @@ List<Task> tasks = [
       time: "10am",
       color: Colors.cyan,
       hangouts: [],
-      finished: false),
+      finished: true),
 ];
 
 class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
   final double imageHeight = 280.0;
-  ScrollController scrollController;
   AnimationController animationController;
   ValueNotifier<bool> shouldOnlyCompletedBeShownNotifier =
       new ValueNotifier<bool>(false);
@@ -71,7 +71,6 @@ class _MyHomePageState extends State<MyHomePage>
     super.initState();
     animationController = new AnimationController(
         vsync: this, duration: const Duration(seconds: 1));
-    scrollController = new ScrollController();
   }
 
   @override
@@ -118,8 +117,6 @@ class _MyHomePageState extends State<MyHomePage>
   Widget _buildTasks() {
     return new Expanded(
       child: new ListView(
-        shrinkWrap: true,
-        controller: scrollController,
         children: tasks
             .map((task) => new TaskRow(
                   showCompletedNotifier: shouldOnlyCompletedBeShownNotifier,
@@ -127,6 +124,7 @@ class _MyHomePageState extends State<MyHomePage>
                   speedFactor: tasks.indexOf(task) / tasks.length,
                 ))
             .toList(),
+        shrinkWrap: true,
       ),
     );
   }
@@ -191,7 +189,8 @@ class _MyHomePageState extends State<MyHomePage>
     return new Positioned(
       top: imageHeight - 100.0,
       right: -40.0,
-      child: new FilterFab(filterValueNotifier: shouldOnlyCompletedBeShownNotifier),
+      child: new FilterFab(
+          filterValueNotifier: shouldOnlyCompletedBeShownNotifier),
     );
   }
 
@@ -243,273 +242,6 @@ class _MyHomePageState extends State<MyHomePage>
       ),
     );
   }
-}
-
-class FilterFab extends StatefulWidget {
-  final ValueNotifier<bool> filterValueNotifier;
-
-  const FilterFab({Key key, this.filterValueNotifier}) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() {
-    return new FilterFabState();
-  }
-}
-
-class FilterFabState extends State<FilterFab>
-    with SingleTickerProviderStateMixin {
-  AnimationController animationController;
-  Animation<double> animation;
-  Animation<Color> colorAnimation;
-
-  final double expandedSize = 180.0;
-  final double hiddenSize = 20.0;
-
-  @override
-  void initState() {
-    super.initState();
-    animationController = new AnimationController(
-        vsync: this, duration: Duration(milliseconds: 200));
-    animation = new CurvedAnimation(
-        parent: animationController, curve: Curves.easeInOut);
-    colorAnimation = new ColorTween(begin: Colors.pink, end: Colors.pink[800])
-        .animate(animationController);
-  }
-
-  @override
-  void dispose() {
-    animationController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new SizedBox(
-      width: expandedSize,
-      height: expandedSize,
-      child: new AnimatedBuilder(
-          animation: animation,
-          builder: (context, child) {
-            return new Stack(
-              alignment: Alignment.center,
-              children: <Widget>[
-                _buildExpandedBackground(),
-                buildOption(Icons.check_circle, 0.0),
-                buildOption(Icons.flash_on, -math.pi / 3),
-                buildOption(Icons.access_time, -2 * math.pi / 3),
-                buildOption(Icons.error_outline, math.pi),
-                _buildFabCore(),
-              ],
-            );
-          }),
-    );
-  }
-
-  open() {
-    if (animationController.isDismissed) {
-      animationController.forward();
-    }
-  }
-
-  close() {
-    if (animationController.isCompleted) {
-      animationController.reverse();
-    }
-  }
-
-  _onFabTap() {
-    if (animation.isDismissed) {
-      open();
-    } else {
-      close();
-    }
-  }
-
-  _onItemTap() {
-    widget.filterValueNotifier.value = !widget.filterValueNotifier.value;
-    close();
-  }
-
-  Widget _buildExpandedBackground() {
-    return new Container(
-      height: hiddenSize + (expandedSize - hiddenSize) * animation.value,
-      width: hiddenSize + (expandedSize - hiddenSize) * animation.value,
-      decoration: new BoxDecoration(shape: BoxShape.circle, color: Colors.pink),
-    );
-  }
-
-  Widget _buildFabCore() {
-    double defaultIconSize = 26.0;
-    double scaleFactor = 2 * (animationController.value - 0.5).abs();
-    return new FloatingActionButton(
-      onPressed: _onFabTap,
-      backgroundColor: colorAnimation.value,
-      child: new Center(
-        child: new Transform(
-          alignment: Alignment.center,
-          transform: new Matrix4.identity()..scale(1.0, scaleFactor),
-          child: new Icon(
-            animationController.value > 0.5 ? Icons.close : Icons.filter_list,
-            color: Colors.white,
-            size: defaultIconSize,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget buildOption(IconData icon, double angle) {
-    double iconSize = 0.0;
-    if (animation.value > 0.5) {
-      iconSize = 26 * animation.value;
-    }
-    double defaultPadding = 16.0;
-    double padding = defaultPadding +
-        (expandedSize - defaultPadding) * (1 - animation.value);
-    return new Transform.rotate(
-      angle: angle,
-      child: new Align(
-        alignment: Alignment.topCenter,
-        child: new Padding(
-          padding: new EdgeInsets.only(top: padding),
-          child: new IconButton(
-            onPressed: _onItemTap,
-            icon: new Transform.rotate(
-              angle: -angle,
-              child: new Icon(
-                icon,
-                color: Colors.white,
-              ),
-            ),
-            iconSize: iconSize,
-            alignment: Alignment.topCenter,
-            padding: new EdgeInsets.all(0.0),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class TaskRow extends StatefulWidget {
-  final double speedFactor;
-  final ValueNotifier<bool> showCompletedNotifier;
-  final Task task;
-
-  const TaskRow(
-      {Key key, this.showCompletedNotifier, this.task, this.speedFactor})
-      : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() {
-    return new TaskRowState();
-  }
-}
-
-class TaskRowState extends State<TaskRow> with TickerProviderStateMixin {
-  AnimationController animationController;
-  Animation animation;
-  bool shouldBeShown;
-  VoidCallback onFilterChanged;
-
-  @override
-  void initState() {
-    super.initState();
-    shouldBeShown = widget.task.finished || !widget.showCompletedNotifier.value;
-    animationController = new AnimationController(
-        value: 1.0,
-        vsync: this,
-        duration: new Duration(
-            milliseconds: (200 * (1 + 2 * widget.speedFactor)).toInt()));
-    animation = new CurvedAnimation(
-        parent: animationController, curve: Curves.easeInOut);
-    onFilterChanged = () {
-      bool shouldOnlyCompletedBeShown = widget.showCompletedNotifier.value;
-      bool shouldThisBeShown =
-          widget.task.finished || !shouldOnlyCompletedBeShown;
-      if (shouldThisBeShown != shouldBeShown) {
-        if (shouldThisBeShown) {
-          animationController
-              .forward()
-              .then((_) => setState(() => shouldBeShown = shouldThisBeShown));
-        } else {
-          animationController
-              .reverse()
-              .then((_) => setState(() => shouldBeShown = shouldThisBeShown));
-        }
-      }
-    };
-    widget.showCompletedNotifier.addListener(onFilterChanged);
-  }
-
-  @override
-  void dispose() {
-    animationController.dispose();
-    widget.showCompletedNotifier.removeListener(onFilterChanged);
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    double dotSize = 12.0;
-    return new AnimatedBuilder(
-      animation: animation,
-      builder: (context, child) => new Opacity(
-            opacity: animation.value,
-            child: new SizeTransition(
-              sizeFactor: animation,
-              child: new Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: new Row(
-                  children: <Widget>[
-                    new Padding(
-                      padding: new EdgeInsets.symmetric(
-                          horizontal: 32.0 - dotSize / 2),
-                      child: new Container(
-                        height: dotSize,
-                        width: dotSize,
-                        decoration: new BoxDecoration(
-                            shape: BoxShape.circle, color: widget.task.color),
-                      ),
-                    ),
-                    new Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        new Text(
-                          widget.task.name,
-                          style: new TextStyle(fontSize: 18.0),
-                        ),
-                        new Text(
-                          widget.task.category,
-                          style:
-                              new TextStyle(fontSize: 12.0, color: Colors.grey),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-    );
-  }
-}
-
-class Task {
-  final String name;
-  final String category;
-  final String time;
-  final List<String> hangouts;
-  final Color color;
-  final bool finished;
-
-  Task(
-      {this.name,
-      this.category,
-      this.time,
-      this.hangouts,
-      this.color,
-      this.finished});
 }
 
 class LineClipper extends CustomClipper<Path> {
