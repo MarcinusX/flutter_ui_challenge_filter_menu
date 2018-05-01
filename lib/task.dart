4b19bbea8c4020857d1f10978522258224c21e59
Marcin Szalek <marcin.szalek1@gmail.com>
2018-05-01 13:02:47 +0200

    Task class

---
+++ lib/task.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Task {
+  final String name;
+  final String category;
+  final String time;
+  final Color color;
+  final bool completed;
+
+  Task({this.name, this.category, this.time, this.color, this.completed});
+}
+  
