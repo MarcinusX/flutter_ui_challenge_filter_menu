01de7696d9cdb3a41c6749dbed949061824737ec
Marcin Szalek <marcin.szalek1@gmail.com>
2018-05-01 12:38:34 +0200

    Profile row

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,7 @@ class _MainPageState extends State<MainPage> {
         children: <Widget>[
           _buildIamge(),
           _buildTopHeader(),
+          _buildProfileRow(),
         ],
       ),
     );
@@ -70,6 +71,44 @@ class _MainPageState extends State<MainPage> {
             ),
           ),
           new Icon(Icons.linear_scale, color: Colors.white),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProfileRow() {
+    return new Padding(
+      padding: new EdgeInsets.only(left: 16.0, top: _imageHeight / 2.5),
+      child: new Row(
+        children: <Widget>[
+          new CircleAvatar(
+            minRadius: 28.0,
+            maxRadius: 28.0,
+            backgroundImage: new AssetImage('images/avatar.jpg'),
+          ),
+          new Padding(
+            padding: const EdgeInsets.only(left: 16.0),
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                new Text(
+                  'Ryan Barnes',
+                  style: new TextStyle(
+                      fontSize: 26.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400),
+                ),
+                new Text(
+                  'Product designer',
+                  style: new TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
